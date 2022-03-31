24389f5621d27c530e7836835dcface3bbcd8e69
Charles Hill <chill@degreesofzero.com>
2022-03-31 13:42:51 +0100

    More fiat currencies
    Minor changes in data structure of fiatCurrencies array

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![Build Status](https://github.com/bleskomat/bleskomat.conf/actions/workflows/tests.yml/badge.svg)
 
-Node.js module to read/write bleskomat.conf files and parse/stringify bleskomat key-value objects/strings. Also includes recommended fiat currency configurations.
+Node.js module to read/write bleskomat.conf files and parse/stringify bleskomat.conf key-value objects/strings. Also includes recommended fiat currency configurations.
 
 * [Installation](#installation)
 * [Usage](#usage)
@@ -91,24 +91,20 @@ Result:
 [
 	// ..
 	{
-		"symbol": "CZK",
+		"fiatCurrency": "CZK",
 		"coinValues": [1, 2, 5, 10, 20, 50],
 		"coinValueIncrement": 1,
 		"billValues": [100, 200, 500, 1000, 2000],
 		"fiatPrecision": 0,
-		"defaults": {
-			"buyLimit": 20000
-		}
+		"buyLimit": 20000
 	},
 	{
-		"symbol": "EUR",
+		"fiatCurrency": "EUR",
 		"coinValues": [0.05, 0.10, 0.20, 0.50, 1.00, 2.00],
 		"coinValueIncrement": 0.05,
 		"billValues": [5, 10, 20, 50, 100, 200],
 		"fiatPrecision": 2,
-		"defaults": {
-			"buyLimit": 1000
-		}
+		"buyLimit": 1000
 	}
 	// ..
 ]
--- test/unit/lib/fiatCurrencies.js
+++ test/unit/lib/fiatCurrencies.js
@@ -6,19 +6,17 @@ describe('fiatCurrencies', function() {
 	it('sanity check', function() {
 		assert.ok(fiatCurrencies instanceof Array);
 		assert.ok(fiatCurrencies.length > 0);
-		fiatCurrencies.forEach((fiatCurrency, index) => {
-			assert.strictEqual(typeof fiatCurrency, 'object');
-			const { symbol } = fiatCurrency;
-			assert.ok(symbol, `Missing "symbol" for fiat currency where index = ${index}`);
-			assert.strictEqual(symbol.length, 3, `Fiat currency symbol must be exactly 3 characters: symbol = "${symbol}"`);
-			assert.strictEqual(symbol, symbol.replace(/[^A-Z]/g, ''), `Fiat currency symbol must contain only uppercase alphabetic characters: symbol = "${symbol}"`);
-			assert.ok(fiatCurrency.coinValues, `Missing "coinValues" for fiat currency where symbol = ${symbol}`);
-			assert.ok(fiatCurrency.coinValueIncrement, `Missing "coinValueIncrement" for fiat currency where symbol = ${symbol}`);
-			assert.ok(fiatCurrency.billValues, `Missing "billValues" for fiat currency where symbol = ${symbol}`);
-			assert.notStrictEqual(typeof fiatCurrency.fiatPrecision, 'undefined', `Missing "fiatPrecision" for fiat currency where symbol = ${symbol}`);
-			assert.ok(fiatCurrency.defaults, `Missing "defaults" for fiat currency where symbol = ${symbol}`);
-			assert.strictEqual(typeof fiatCurrency.defaults, 'object');
-			assert.ok(fiatCurrency.defaults.buyLimit, `Missing "defaults.buyLimit" for fiat currency where symbol = ${symbol}`);
+		fiatCurrencies.forEach((item, index) => {
+			assert.strictEqual(typeof item, 'object');
+			const { fiatCurrency } = item;
+			assert.ok(fiatCurrency, `Missing currency symbol ("fiatCurrency"): index = ${index}`);
+			assert.strictEqual(fiatCurrency.length, 3, `Fiat currency symbol must be exactly 3 characters: fiatCurrency = "${fiatCurrency}"`);
+			assert.strictEqual(fiatCurrency, fiatCurrency.replace(/[^A-Z]/g, ''), `Fiat currency symbol must contain only uppercase alphabetic characters: fiatCurrency = "${fiatCurrency}"`);
+			assert.ok(item.coinValues, `Missing "coinValues" for fiat currency where fiatCurrency = ${fiatCurrency}`);
+			assert.ok(item.coinValueIncrement, `Missing "coinValueIncrement" for fiat currency where fiatCurrency = ${fiatCurrency}`);
+			assert.ok(item.billValues, `Missing "billValues" for fiat currency where fiatCurrency = ${fiatCurrency}`);
+			assert.notStrictEqual(typeof item.fiatPrecision, 'undefined', `Missing "fiatPrecision" for fiat currency where fiatCurrency = ${fiatCurrency}`);
+			assert.ok(item.buyLimit, `Missing "buyLimit" for fiat currency where fiatCurrency = ${fiatCurrency}`);
 		});
 	});
 });
