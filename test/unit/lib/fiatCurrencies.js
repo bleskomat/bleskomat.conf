839bc776ac62f6e552605c154558e23558dd37da
Charles Hill <chill@degreesofzero.com>
2023-10-13 12:11:19 +0100

    Added more currencies
    More test cases to sanity check fiat currencies

--- test/unit/lib/fiatCurrencies.js
+++ test/unit/lib/fiatCurrencies.js
@@ -3,20 +3,67 @@ const { fiatCurrencies } = require('../../../');
 
 describe('fiatCurrencies', function() {
 
-	it('sanity check', function() {
+	it('sanity checks', function() {
 		assert.ok(fiatCurrencies instanceof Array);
 		assert.ok(fiatCurrencies.length > 0);
-		fiatCurrencies.forEach((item, index) => {
-			assert.strictEqual(typeof item, 'object');
-			const { fiatCurrency } = item;
-			assert.ok(fiatCurrency, `Missing currency symbol ("fiatCurrency"): index = ${index}`);
-			assert.strictEqual(fiatCurrency.length, 3, `Fiat currency symbol must be exactly 3 characters: fiatCurrency = "${fiatCurrency}"`);
-			assert.strictEqual(fiatCurrency, fiatCurrency.replace(/[^A-Z]/g, ''), `Fiat currency symbol must contain only uppercase alphabetic characters: fiatCurrency = "${fiatCurrency}"`);
-			assert.ok(item.coinValues, `Missing "coinValues" for fiat currency where fiatCurrency = ${fiatCurrency}`);
-			assert.ok(item.coinValueIncrement, `Missing "coinValueIncrement" for fiat currency where fiatCurrency = ${fiatCurrency}`);
-			assert.ok(item.billValues, `Missing "billValues" for fiat currency where fiatCurrency = ${fiatCurrency}`);
-			assert.notStrictEqual(typeof item.fiatPrecision, 'undefined', `Missing "fiatPrecision" for fiat currency where fiatCurrency = ${fiatCurrency}`);
-			assert.ok(item.buyLimit, `Missing "buyLimit" for fiat currency where fiatCurrency = ${fiatCurrency}`);
+	});
+
+	fiatCurrencies.forEach((item, index) => {
+
+		const { fiatCurrency } = item || {};
+
+		describe(`"${fiatCurrency}" (index = ${index})`, function() {
+
+			describe('fiatCurrency symbol', function() {
+				it('is exactly 3 characters', function() {
+					assert.strictEqual(fiatCurrency.length, 3);
+				});
+				it('contains only uppercase alphabetic characters', function() {
+					assert.strictEqual(fiatCurrency, fiatCurrency.replace(/[^A-Z]/g, ''));
+				});
+			});
+
+			['fiatCurrency', 'coinValues', 'coinValueIncrement', 'billValues', 'fiatPrecision', 'buyLimit'].forEach(key => {
+				it(`"${key}" is defined`, function() {
+					assert.notStrictEqual(typeof item[key], 'undefined');
+				});
+			});
+
+			['coinValues', 'billValues'].forEach(key => {
+				describe(key, function() {
+					it('is an array of numbers', function() {
+						assert.ok(item[key] instanceof Array);
+						item[key].forEach(value => {
+							assert.ok(!Number.isNaN(parseFloat(value)));
+						});
+					});
+					it('is in ascending order', function() {
+						item[key].forEach((value, index) => {
+							assert.ok(index === 0 || value > item[key][index - 1]);
+						});
+					});
+				});
+			});
+
+			['coinValueIncrement', 'buyLimit'].forEach(key => {
+				it(`"${key}" is a number`, function() {
+					assert.ok(!Number.isNaN(parseFloat(item[key])));
+				});
+			});
+
+			['fiatPrecision'].forEach(key => {
+				it(`"${key}" is an integer`, function() {
+					assert.ok(Number.isInteger(item[key]));
+				});
+			});
+
+			it('all numbers in "coinValues" are divisible by "coinValueIncrement"', function() {
+				const multiplier = Math.pow(10, item.fiatPrecision);
+				const increment = item.coinValueIncrement * multiplier;
+				item.coinValues.forEach((value, index) => {
+					assert.ok((value * multiplier) % increment === 0);
+				});
+			});
 		});
 	});
 });
